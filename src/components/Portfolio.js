3f682a72c0f23ca7064378a10666a0cd5539e5e1
Amanda Gorfine <amandagorfine@Amandas-MacBook-Pro-2.local>
2018-12-15 02:21:58 -0500

    updated portfolio to include disney dates and two photographs for mobile apps

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -28,8 +28,9 @@ function Portfolio (props) {
 				</div>
 				<div className = 'portfolioProject'>
 					<img className = 'project3' src="https://res.cloudinary.com/drsaojfyp/image/upload/v1542481614/Screen_Shot_2018-11-17_at_1.57.52_PM.png" alt= "project1"/>
+					<img className = 'project3' src="https://res.cloudinary.com/drsaojfyp/image/upload/v1544857724/Screen_Shot_2018-12-15_at_1.29.15_AM.png" alt= "project1"/>
 					<div className = 'projectDescription'>
-						<div className = 'projectTitles'>Instagram  |  React, CSS, JavaScript</div>
+						<div className = 'projectTitles'>Instagram  |  React, Express, CSS, JavaScript</div>
 						<div className = 'projectInformation'>For my third project, I worked with two of my classmates to recreate the Instagram mobile application. Users can create an account, and post photographs or capture photographs directly from their computer. Users are also able to view other users photographs through the newsfeed.</div>
 						<div className = 'linkBox'>
 							<a className = 'gitLink' href="https://github.com/agorfine/Instagram" target="_blank" rel="noopener noreferrer">View my Code </a>
@@ -38,16 +39,26 @@ function Portfolio (props) {
 				</div>
 				<div className = 'portfolioProject'>
 					<div className = 'projectDescription'>
+						<div className = 'projectTitles'>Disney Dates  |  React Native, Express, FaceBook SDK, Firebase, CSS, JavaScript</div>
+						<div className = 'projectInformation'>For my fourth project, I created Disney Dates- a dating mobile application for people who share a mutual love for all things Disney. Users can create an account through FaceBook. Once a new user logs in, they are directed to the settings page to personlize their profile. Once the user completes their profile, they can swipe through other users until they find their perfect match.</div>
+						<div className = 'linkBox'>
+							<a className = 'gitLink' href="https://github.com/agorfine/DisneyDates" target="_blank" rel="noopener noreferrer">View my Code </a>
+						</div>
+					</div>
+					<img className = 'projectDisneyDates' src="https://res.cloudinary.com/drsaojfyp/image/upload/v1544857452/IMG_A9A42B16874C-1.jpg" alt= "project1"/>
+					<img className = 'projectDisneyDates' src="https://res.cloudinary.com/drsaojfyp/image/upload/v1544857428/IMG_A4DB8BC0C1C8-1.jpg" alt= "project1"/>
+				</div>
+				<div className = 'portfolioProject'>
+					<img className = 'projectPic project4' src="https://res.cloudinary.com/drsaojfyp/image/upload/v1544733270/Screen_Shot_2018-12-13_at_3.34.17_PM.png" alt= "project1"/>
+					<div className = 'projectDescription'>
 						<div className = 'projectTitles'>Tic-Tac-Toe  |  HTML, CSS, JavaScript</div>
 						<div className = 'projectInformation'>For a homework assignment, we were tasked to create a tic-tac-toe game. Users must click on a square to render an X for player 1 and an O for player 2.</div>
 						<div className = 'linkBox'>
 							<a className = 'gitLink' href="https://github.com/agorfine/tic-tac-toe" target="_blank" rel="noopener noreferrer">View my Code </a>
 						</div>
 					</div>
-					<img className = 'projectPic project4' src="https://res.cloudinary.com/drsaojfyp/image/upload/v1544733270/Screen_Shot_2018-12-13_at_3.34.17_PM.png" alt= "project1"/>
 				</div>
 				<div className = 'portfolioProject'>
-					<img className = 'projectPic project4' src="https://res.cloudinary.com/drsaojfyp/image/upload/v1542481645/Screen_Shot_2018-11-17_at_1.59.01_PM.png" alt= "project1"/>
 					<div className = 'projectDescription'>
 						<div className = 'projectTitles'>Duck Hunt  |  JavaScript</div>
 						<div className = 'projectInformation'>For a homework assignment, we were tasked to write the JavaScript logic to make a working Duck Hunt game. Users must click on all of the moving ducks to win the game.</div>
@@ -55,8 +66,10 @@ function Portfolio (props) {
 							<a className = 'gitLink' href="https://github.com/agorfine/Duck-Hunt" target="_blank" rel="noopener noreferrer">View my Code </a>
 						</div>
 					</div>
+					<img className = 'projectPic project4' src="https://res.cloudinary.com/drsaojfyp/image/upload/v1542481645/Screen_Shot_2018-11-17_at_1.59.01_PM.png" alt= "project1"/>
 				</div>
 				<div className = 'portfolioProject'>
+					<img className = 'projectPic' src="https://res.cloudinary.com/drsaojfyp/image/upload/v1542481182/Screen_Shot_2018-11-17_at_1.59.28_PM.png" alt= "project1"/>
 					<div className = 'projectDescription'>
 						<div className = 'projectTitles'>Weather App  |  HTML, CSS, JavaScript, API</div>
 						<div className = 'projectInformation'>For a homework assignment, I created a weather website. Users can type in their zipcode and view the current weather. </div>
@@ -64,7 +77,6 @@ function Portfolio (props) {
 							<a className = 'gitLink' href="https://github.com/agorfine/JavaScript-Weather-App" target="_blank" rel="noopener noreferrer">View my Code </a>
 						</div>
 					</div>
-					<img className = 'projectPic project5' src="https://res.cloudinary.com/drsaojfyp/image/upload/v1542481182/Screen_Shot_2018-11-17_at_1.59.28_PM.png" alt= "project1"/>
 				</div>
 			</div>	
 		</div>
