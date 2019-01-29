27232bc057b6e9b201b20ee9bb6b31512bb0614b
Amanda Gorfine <amandagorfine@Amandas-MacBook-Pro-2.local>
2019-01-29 11:35:48 -0500

    fixed about me img bug

---
+++ src/components/untitled.js
@@ -0,0 +1,8 @@
+
+/*<div>
+						<div className='aboutMeTitle'>Places . . .</div>
+						<div className = 'aboutmeInfo'>I am currently looking for full-time employment in the Metro-NY area.</div>
+						<div className = 'map'>
+							<Map viewport= { viewport } mobile = {viewportMobile} />
+						</div>
+					</div> */
